23c3f560136de1064ca20072a1b868f4556c1cff
nicohasa <nicohasa@gmail.com>
2017-02-04 13:41:42 +0100

    Added superscripts

--- abbreviations.py
+++ abbreviations.py
@@ -238,3 +238,18 @@ ABBREVIATIONS = {
 	'potato': 'Jinteki: Potential Unleashed',
 	'nikolai': 'Jesminder Sareen: Girl Behind The Curtain'
 }
+
+SUPERSCRIPTS = {
+		'0': '⁰',
+		'1': '¹',
+		'2': '²',
+		'3': '³',
+		'4': '⁴',
+		'5': '⁵',
+		'6': '⁶',
+		'7': '⁷',
+		'8': '⁸',
+		'9': '⁹',
+		'X': 'ˣ',
+		'x': 'ˣ'
+	}
